ccec1f3ab16b97b5478ff8e888b9f38f3778f00d
Muhammed  Ahad <muhammed.ahad@Ahads-MacBook.local>
2024-10-10 10:19:08 +0200

    nvim : remove mini-surround

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
-    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     -- import/override with your plugins
     { import = "plugins" },
