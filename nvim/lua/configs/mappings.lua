40744ef954e064c2a2a747654017993f23f0dead
Muhammed  Ahad <maaahad@gmail.com>
2024-12-15 13:42:58 +0100

    nvim: minimalistic setup initiated

---
+++ nvim/lua/configs/mappings.lua
@@ -0,0 +1,5 @@
+
+local keymap = vim.keymap
+
+-- Exit insert mode
+keymap.set("i", "kk", "<Esc>") 
---
+++ nvim/lua/configs/options.lua
@@ -0,0 +1,11 @@
+
+-- leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true 		-- display line number
+opt.relativenumber = true 	-- relative line number
