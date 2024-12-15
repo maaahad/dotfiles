869127f7a59d4e7f6381aeccd7a22eefbaf9cd32
Muhammed  Ahad <maaahad@gmail.com>
2024-12-15 23:42:53 +0100

    nvim : fterm

---
+++ nvim/lua/plugins/fterm.lua
@@ -0,0 +1,9 @@
+return {
+	'numToStr/FTerm.nvim',
+	config = function()
+		local fterm = require('FTerm')
+		fterm.setup()
+		vim.keymap.set('n', '<leader>tt', function() fterm:toggle() end, {desc = '[T]oggle [T]terminal'})
+		vim.keymap.set('t', '<leader>tt', function() fterm:toggle() end, {desc = '[T]oggle [T]erminal'})
+	end,
+}
