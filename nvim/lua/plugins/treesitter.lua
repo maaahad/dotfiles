099dd2b75ac842c62362ea40d50500d54df90bc7
Muhammed  Ahad <maaahad@gmail.com>
2024-12-16 21:54:14 +0100

    nvim : treesitter

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,49 @@
+return {
+  'nvim-treesitter/nvim-treesitter', 
+  build = ":TSUpdate", 
+  config = function()
+    local configs = require('nvim-treesitter.configs')
+
+    configs.setup({
+      ensure_installed = {
+        'lua', 
+        'vim', 
+        'markdown', 
+        'markdown_inline', 
+        'css', 
+        'json', 
+        'javascript', 
+        'typescript', 
+        'tsx', 
+        'yaml', 
+        'graphql', 
+        'gitignore', 
+      }, 
+
+      highlight = {
+        enable = true, 
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      }, 
+
+      indent = {
+        enable = true, 
+      },
+
+      -- TODO: Remove this if not being used
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+
+    })
+  end
+}
