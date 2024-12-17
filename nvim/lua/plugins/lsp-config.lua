90af337172725f406c32f8a7aba0c4b6f31a5be7
Muhammed  Ahad <maaahad@gmail.com>
2024-12-17 22:13:22 +0100

    nvim : setup lsp-config and nvim-cmp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -4,4 +4,37 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
   },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    require("mason-lspconfig").setup_handlers({
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+    })
+
+    -- Global LSP mappings
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+    -- More LSP mappings
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<space>.", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      end,
+    })
+  end
 }
--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,10 +1,64 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-  },
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	config = function()
+		-- Set up nvim-cmp
+		-- TODO: Uncomment autopairs related stuff if decided to use autopairs
+		-- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		local luasnip = require("luasnip")
+		local cmp = require("cmp")
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+			sources = {
+				{ name = "nvim_lsp", max_item_count = 5 },
+				{ name = "buffer", max_item_count = 5 },
+				{ name = "path", max_item_count = 3 },
+				{ name = "luasnip", max_item_count = 3 },
+			},
+			formatting = {
+				format = function(_, vim_item)
+					vim_item.abbr = string.sub(vim_item.abbr, 1, 20)
+					return vim_item
+				end,
+			},
+		})
+
+		-- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+	end,
 }
