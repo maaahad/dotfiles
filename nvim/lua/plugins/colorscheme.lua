b7183da384782f66cf61ca94b66bf9fc05b857be
Muhammed  Ahad <maaahad@gmail.com>
2024-12-18 17:30:12 +0100

    nvin : solarized theme added

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,25 @@
-return {
-  'rebelot/kanagawa.nvim', 
-  config = function()
-    require('kanagawa').setup({})
+-- kanagawa
+-- return {
+--   'rebelot/kanagawa.nvim',
+--   config = function()
+--     require('kanagawa').setup({})
+--
+--     -- setting up kanagawa as default colorscheme
+--     vim.cmd("colorscheme kanagawa")
+--   end
+-- }
 
-    -- setting up kanagawa as default colorscheme
-    vim.cmd("colorscheme kanagawa")
-  end
+-- Solarized
+return {
+	"maxmx03/solarized.nvim",
+	-- lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function(_, opts)
+		vim.o.termguicolors = true
+		-- vim.o.background = "light"
+		vim.o.background = "dark"
+		require("solarized").setup(opts)
+		vim.cmd.colorscheme("solarized")
+	end,
 }
