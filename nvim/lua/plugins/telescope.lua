d98d9ff7b1ba866f42271cb1c6eb14b24d9094b4
Muhammed  Ahad <maaahad@gmail.com>
2024-12-21 20:36:00 +0100

    nvim : update treesitter and telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,13 +1,44 @@
 -- TODO: will might ditch this onec happy with fzf-lua
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-	end,
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  },
+  config = function()
+    require("telescope").setup({
+      -- TODO: set the same theme all
+      pickers = {
+        -- cursor, dropdown, ivy
+        find_files = {
+          theme = "ivy",
+        },
+        live_grep = {
+          theme = "ivy",
+        },
+        grep_string = {
+          theme = "ivy",
+        },
+        buffers = {
+          theme = "ivy",
+        },
+        help_tags = {
+          theme = "ivy",
+        },
+      },
+      extensions = {
+        fzf = {},
+      },
+    })
+
+    require("telescope").load_extension("fzf")
+
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+    vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Telescope grep for string under the cursor" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+  end,
 }
