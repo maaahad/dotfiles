2ba214ae594c3053f6c74c97df6e9009d0891c12
Muhammed  Ahad <maaahad@gmail.com>
2024-12-22 17:46:27 +0100

    nvim : removed old terminal keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,12 +2,3 @@ local keymap = vim.keymap
 
 -- Exit insert mode
 keymap.set("i", "kk", "<esc>")
-
--- Terminal
--- TODO: if happy remap st to tt
-vim.keymap.set("n", "<leader>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
-end)
