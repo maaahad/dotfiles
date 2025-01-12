3ff50d23e8540f723beae807309f5a2902ee18c8
Muhammed  Ahad <maaahad@gmail.com>
2025-01-12 23:12:38 +0100

    nvim : more colorscheme added

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,35 @@
 -- kanagawa
--- return {
---   'rebelot/kanagawa.nvim',
---   config = function()
---     require('kanagawa').setup({})
---
---     -- setting up kanagawa as default colorscheme
---     vim.cmd("colorscheme kanagawa")
---   end
--- }
-
--- Solarized
 return {
-	"maxmx03/solarized.nvim",
-	-- lazy = false,
-	priority = 1000,
-	opts = {
-		-- "spring" | "summer" | "autumn" | "winter" (default)
-		variant = "spring",
+	{
+		"maxmx03/solarized.nvim",
+		-- lazy = false,
+		priority = 1000,
+		opts = {
+			-- "spring" | "summer" | "autumn" | "winter" (default)
+			variant = "spring",
+		},
+		config = function(_, opts)
+			vim.o.termguicolors = true
+			-- vim.o.background = "light"
+			vim.o.background = "dark"
+
+			-- Setting solarized as default colorscheme
+			require("solarized").setup(opts)
+			vim.cmd.colorscheme("solarized")
+		end,
 	},
-	config = function(_, opts)
-		vim.o.termguicolors = true
-		-- vim.o.background = "light"
-		vim.o.background = "dark"
-		require("solarized").setup(opts)
-		vim.cmd.colorscheme("solarized")
-	end,
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		config = function(opts)
+			require("kanagawa").setup(opts)
+		end,
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
