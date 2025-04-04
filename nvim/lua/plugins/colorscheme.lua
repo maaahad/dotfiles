139602d2893ee69cbf1635b7c0b8ee4fdf78059c
Muhammed  Ahad <maaahad@gmail.com>
2025-04-04 17:14:27 +0200

    nvim : nordic theme addeed

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -41,4 +41,16 @@ return {
 			require("kanagawa").setup(opts)
 		end,
 	},
+	{
+		"andersevenrud/nordic.nvim",
+		config = function()
+			require("nordic").colorscheme({
+				underline_option = "none",
+				italic = true,
+				italic_comments = false,
+				minimal_mode = false,
+				alternate_backgrounds = false,
+			})
+		end,
+	},
 }
