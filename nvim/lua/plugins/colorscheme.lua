6e82e0b050a04860143e9aedc13fea3c58082618
Muhammed  Ahad <maaahad@gmail.com>
2025-04-05 10:57:04 +0200

    nvim : italic set to false for nordic

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -46,7 +46,7 @@ return {
 		config = function()
 			require("nordic").colorscheme({
 				underline_option = "none",
-				italic = true,
+				-- italic = true,
 				italic_comments = false,
 				minimal_mode = false,
 				alternate_backgrounds = false,
