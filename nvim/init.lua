c2c9819e9f576ab2ed75a995cf4db31f90b04f45
Muhammed  Ahad <maaahad@gmail.com>
2025-04-17 15:48:12 +0200

    kickstart-nvim : neotest

--- nvim/init.lua
+++ nvim/init.lua
@@ -1067,6 +1067,31 @@ require('lazy').setup({
     'kristijanhusak/vim-dadbod-completion',
     'kristijanhusak/vim-dadbod-ui',
   },
+  -- Test with neotest, (golang and vitest)
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'marilari88/neotest-vitest',
+      'fredrikaverpil/neotest-golang',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-vitest' {
+            -- Filter directories when searching for test files. Useful in large projects
+            fioter_dir = function(name, rel_path, _root)
+              return name ~= 'node_modules'
+            end,
+          },
+          require 'neotest-golang',
+        },
+      }
+    end,
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
