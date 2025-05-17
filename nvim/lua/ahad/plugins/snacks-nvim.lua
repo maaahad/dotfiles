c0f8e18f2e4939f6da024afbac90c9f225e8eb8d
Muhammed Ahad <maaahad@gmail.com>
2025-05-17 16:59:11 +0200

    termial toggle

--- nvim/lua/ahad/plugins/snacks-nvim.lua
+++ nvim/lua/ahad/plugins/snacks-nvim.lua
@@ -24,6 +24,44 @@ return {
       },
     },
 
+    -- terminal
+    terminal = {
+      bo = {
+        filetype = 'snacks_terminal',
+      },
+      wo = {},
+      keys = {
+        q = 'hide',
+        gf = function(self)
+          local f = vim.fn.findfile(vim.fn.expand '<cfile>', '**')
+          if f == '' then
+            Snacks.notify.warn 'No file under cursor'
+          else
+            self:hide()
+            vim.schedule(function()
+              vim.cmd('e ' .. f)
+            end)
+          end
+        end,
+        term_normal = {
+          '<esc>',
+          function(self)
+            self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+            if self.esc_timer:is_active() then
+              self.esc_timer:stop()
+              vim.cmd 'stopinsert'
+            else
+              self.esc_timer:start(200, 0, function() end)
+              return '<esc>'
+            end
+          end,
+          mode = 't',
+          expr = true,
+          desc = 'Double escape to normal mode',
+        },
+      },
+    },
+
     -- dashboard
     dashboard = {
       enabled = true,
@@ -558,11 +596,11 @@ return {
       desc = 'Dismiss All Notifications',
     },
     {
-      '<c-/>',
+      '<leader>tt',
       function()
         Snacks.terminal()
       end,
-      desc = 'Toggle Terminal',
+      desc = '[T]oggle [T]erminal',
     },
     {
       '<c-_>',
