a5c7b700fe9c04db0210683dd3504d336030ee74
Muhammed Ahad <maaahad@gmail.com>
2025-05-25 19:16:26 +0200

    keymap to exit insert mode with jj

--- nvim/init.lua
+++ nvim/init.lua
@@ -182,6 +182,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- exit insert mode with jj
+vim.keymap.set('i', 'jj', '<C-\\><C-n>', { desc = 'Exit insert mode' })
+
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
