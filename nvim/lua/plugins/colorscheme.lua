44752c178a1d2e19bf86fd3f4ccca006a1a4dcc3
Muhammed  Ahad <maaahad@gmail.com>
2025-08-13 16:31:38 +0200

    nvim : gruvbox colorscheme added

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
   {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup()
+      vim.o.background = "light"
+      vim.cmd("colorscheme gruvbox")
+    end,
+  },
+  {
     "EdenEast/nightfox.nvim",
     config = function()
       require("nightfox").setup()
-      vim.cmd("colorscheme nightfox")
     end,
   },
 }
